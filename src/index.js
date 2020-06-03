e4d39e3ef329158e951f6a9be042d3971d49ba94
Alex Snell <alexksnell@gmail>
2020-06-03 16:04:13 -0400

    transfering to github

---
+++ src/index.js
@@ -0,0 +1,41 @@
+﻿require('./models/User');
+require('./models/Track');
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+
+const app = express();
+
+//Associate routes with express.
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+
+//Connect to mongoose cluster
+const mongoURI = 'mongodb+srv://admin:admin@tracker-uzd2m.mongodb.net/tracker?retryWrites=true&w=majority';
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+});
+mongoose.connection.on('connected', () => {
+    console.log('connected to mongo instance');
+});
+mongoose.connection.on('error', err => {
+    console.log('Error: ', err);
+});
+
+//Default route
+app.get('/', requireAuth, (req, res) => {
+    res.send(`Email: ${req.user.email}`);
+});
+
+//Port to listen for requests on.
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
+
---
+++ src/middlewares/requireAuth.js
@@ -0,0 +1,23 @@
+﻿const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+module.exports = (req, res, next) => {
+    const { authorization } = req.headers;
+    // authorization === 'Bearer 8fdfg8dfhg8dhf8wdsdfhsfdhjsdfjs232243jb'
+
+    if (!authorization) {
+        return res.status(401).send({ error: 'You must be logged in. '});
+    };
+
+    const token = authorization.replace('Bearer ', '');
+    jwt.verify(token, 'SUPER_SECRET_KEY', async (err, payload) => {
+        if (err) {
+            return res.status(401).send({ error: 'You must be logged in.' });
+        };
+        const { userId } = payload;
+        const user = await User.findById(userId);
+        req.user = user;
+        next();
+    });
+};
